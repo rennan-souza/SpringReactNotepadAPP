727f3ae92eb99170cc7a1b0a4c8bc9932dbc8d0f
Renan <souza.rnn@gmail.com>
2021-08-20 11:10:13 -0300

    User entity

---
+++ backend/src/main/java/renan/notepadapp/NotepadappApplication.java
@@ -0,0 +1,13 @@
+package renan.notepadapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotepadappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotepadappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/renan/notepadapp/NotepadappApplicationTests.java
@@ -0,0 +1,13 @@
+package renan.notepadapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotepadappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
