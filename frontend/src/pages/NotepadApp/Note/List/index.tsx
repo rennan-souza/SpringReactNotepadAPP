124bc25b187364019fd7b935784c4f4754c9387a
Renan <souza.rnn@gmail.com>
2021-08-24 17:02:50 -0300

    Notes listing

--- frontend/src/pages/NotepadApp/Note/List/index.tsx
+++ frontend/src/pages/NotepadApp/Note/List/index.tsx
@@ -1,13 +1,51 @@
-import { getAuthData, getTokenData } from "../../../../util/requests";
+import { AxiosRequestConfig } from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { getAuthData, getTokenData, requestBackend } from "../../../../util/requests";
+
+type Note = {
+    id: number;
+    title: string;
+    content: string;
+};
 
 const List = () => {
+
+    const [notes, setNotes] = useState<Note[]>();
+
+    useEffect(() => {
+        const params: AxiosRequestConfig = {
+            method: "GET",
+            url: "/notes",
+            withCredentials: true,
+        };
+
+        requestBackend(params).then((response) => {
+            setNotes(response.data);
+        });
+    }, []);
+
     return (
         <div className="container">
-            <p>Notes list</p>
-            <p>Name logged: { getAuthData().userFullName}</p>
-            <p>Email logged: { getTokenData()?.user_name}</p>
+            <a href="#link" className="btn btn-success mb-4">
+                <i className="fas fa-plus mr-2"></i>
+                <strong>NOVA NOTA</strong>
+            </a>
+
+            {notes?.map((note) => (
+                <div className="border-bottom mb-4">
+                    <a href="#link" className="text-dark">
+                        <h3>{note.title}</h3>
+                    </a>
+                    <p className="my-1">
+                        {note.content.length < 100
+                            ? note.content
+                            : note.content.substring(0, 100) + "..."}
+                    </p>
+                </div>
+            ))}
         </div>
-    )
+    );
 }
 
 export default List;
